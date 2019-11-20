30990e0944ed9498102f45b5f76956839c073ae1
Smolevich <smolevich90@gmail.com>
2019-11-20 10:57:26 +0300

    Update repository code

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/smolevich/golang-app
+
+go 1.13
+
+require (
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.0.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/subosito/gotenv v1.2.0
+)
--- main.go
+++ main.go
@@ -3,13 +3,51 @@ package main
 import (
 	"fmt"
 	"os"
+	_ "github.com/lib/pq"
+	"github.com/subosito/gotenv"
+	"log"
+	"github.com/jmoiron/sqlx"
 )
 
 const (
-	envRepo = "GITHUB_REPOSITORY"
+	dsn = "SQLX_POSTGRES_DSN"
 )
 
+var schema = `
+CREATE TABLE IF NOT EXISTS person (
+    first_name text,
+    last_name text,
+    email text
+);
+
+CREATE TABLE IF NOT EXISTS place (
+    country text,
+    city text NULL,
+    telcode integer
+)`
+
+type Person struct {
+    FirstName string `db:"first_name"`
+    LastName  string `db:"last_name"`
+    Email     string
+}
+
 func main() {
-	repo := os.Getenv(envRepo)
-	fmt.Println(repo)
+	gotenv.Load()
+	pgdsn := os.Getenv(dsn)
+	db, err := sqlx.Connect("postgres", pgdsn)
+
+	if err != nil {
+        log.Fatalln(err)
+	}
+	db.MustExec(schema)
+	
+	tx := db.MustBegin()
+    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "Jason", "Moiron", "jmoiron@jmoiron.net")
+    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "John", "Doe", "johndoeDNE@gmail.net")
+    tx.Commit()
+
+	people := []Person{}
+	db.Select(&people, "SELECT * FROM person ORDER BY first_name ASC")
+	fmt.Println(people)
 }
